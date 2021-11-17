41a772d2d059ba5ce131b4a7b0f39275b45bb6df
TheSnewfie <samuel.newfield.19@cnu.edu>
2021-11-17 00:17:47 -0500

    Delete Implemented, minor aesthetic changes

--- html/delete.php
+++ html/delete.php
@@ -1,1 +1,45 @@
-Placeholder delete php file
+<html>
+<head>
+        <title>Delete</title>
+        <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div id="main">
+<br>
+<?php
+//Final Project - B-Team Delete PHP
+$servername = "192.168.0.2";
+$username   = "dev";
+$password   = "password";
+$dbname     = "devDB";
+
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if($conn->connect_error){
+        die("connection failed: ". $conn->connect_error);
+}
+echo "Successful Retrieval Effort<br>";
+$sql = "SELECT *  FROM items WHERE code='".$_GET["ID"]."'";
+$sql2 = "DELETE FROM items WHERE code='".$_GET["ID"]."'";
+
+$result = $conn->query($sql);
+if($result->num_rows > 0){
+	if($conn->query($sql2) === TRUE){
+        	echo "<h2>Record Successfully Deleted</h2><br>";
+	}	
+	else{
+        	echo "ERROR DELETING RECORD: " . $conn->error;
+	}
+}
+else{
+	echo "<h1>No record matches ID: ".$_GET["ID"]."</h1><br>";
+}
+
+?>
+<form action="delete.html">
+    <input type="submit" value="Back to Delete" />
+</form>
+<form action="index.html">
+	<input type="submit" value="Back to Home" />
+</form>
+
--- html/insert.php
+++ html/insert.php
@@ -1,5 +1,13 @@
+<html>
+<head>
+        <title>Insertion/Update</title>
+        <link href="style.css" rel="stylesheet">
+</head>
+<body>
+<div id="main">
+<br>
 <?php
-//Final Project B-Team Connection Sandbox Test
+//Final Project - B-Team Insert PHP
 $servername = "192.168.0.2";
 $username   = "dev";
 $password   = "password";
@@ -10,35 +18,35 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if($conn->connect_error){
         die("connection failed: ". $conn->connect_error);
 }
-echo "<h3>Successful Retrieval Effort</h3><br>";
+echo "Successful Retrieval Effort<br>";
 $sql = "SELECT *  FROM items WHERE code='".$_GET["ID"]."'";
 $sql2 = "INSERT INTO items (code, name, quantity) VALUES ('".$_GET["ID"]."', '".$_GET["Name"]."', '".$_GET["Quantity"]."')";
 $sql3 = "UPDATE items SET name='".$_GET["Name"]."', quantity='".$_GET["Quantity"]."' WHERE code='".$_GET["ID"]."'";
 $result = $conn->query($sql);
 if($result->num_rows > 0){
         if ($conn->query($sql3) === TRUE) {
-		echo "Record updated successfully";
-	}
-	else {
-		echo "ERROR UPDATING RECORD: ".$conn->error;
-	}
+                echo "<h2>Record updated successfully</h2>";
+        }
+        else {
+                echo "ERROR UPDATING RECORD: ".$conn->error;
+        }
 }
 else{
-	if ($conn->query($sql2) === TRUE) {
-		echo "New record created successfully";
-	}
-	else {
-		echo "Error: " . $sql2 . "<br>" . $conn->error;
-	}
+        if ($conn->query($sql2) === TRUE) {
+                echo "New record created successfully<br>";
+        }
+        else {
+                echo "Error: " . $sql2 . "<br>" . $conn->error;
+        }
 
 }
 
 ?>
-
-<br><br>
 <form action="insert.html">
     <input type="submit" value="Back to Insert" />
 </form>
 <form action="index.html">
     <input type="submit" value="Back to Home" />
 </form>
+</body>
+</html>
